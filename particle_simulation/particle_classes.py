a05538012c29ab56a4ef1ff58b4af011e5db02d2
moodydoodi <148434600+moodydoodi@users.noreply.github.com>
2025-02-15 12:28:39 +0100

    changed parameters to work with pygame

--- particle_simulation/particle_classes.py
+++ particle_simulation/particle_classes.py
@@ -1,9 +1,6 @@
 """Particles that use the main particle class
 """
 from particle_simulation.main_classes import Particle, ParticleField
-import matplotlib
-matplotlib.use('TkAgg') 
-
 
 class Particle_A(Particle):
     def __init__(self, position):
@@ -11,7 +8,7 @@ class Particle_A(Particle):
         self.particle_label = "Particle_A"
         self.step_size = 0.2
         self.influence_strength = 0.5
-        self.influence_radius = 1
+        self.influence_radius = 25
         self.color = Particle.generate_particle_colors(self.particle_label, 1)[0] #gets a colorway for this spicific type (red)
         self.shape = Particle.generate_particle_shape(self.particle_label)
 
@@ -20,9 +17,9 @@ class Particle_B(Particle):
     def __init__(self, position):
         super().__init__(position)
         self.particle_label = "Particle_B"
-        self.step_size = -0.2
-        self.influence_strength = 0.5
-        self.influence_radius = 1
+        self.step_size = 0.2
+        self.influence_strength = 1
+        self.influence_radius = 50
         self.color = Particle.generate_particle_colors(self.particle_label, 1)[0]  #gets a colorway for this spicific type
         self.shape = Particle.generate_particle_shape(self.particle_label)
 
@@ -32,8 +29,8 @@ class Particle_C(Particle):
         super().__init__(position)
         self.particle_label = "Particle_C"
         self.step_size = 0.2
-        self.influence_strength = 0.5
-        self.influence_radius = 1
+        self.influence_strength = 5
+        self.influence_radius = 75
         self.color = Particle.generate_particle_colors(self.particle_label, 1)[0]  #gets a colorway for this spicific type
         self.shape = Particle.generate_particle_shape(self.particle_label)
 
@@ -42,9 +39,9 @@ class Particle_D(Particle):
     def __init__(self, position):
         super().__init__(position)
         self.particle_label = "Particle_D"
-        self.step_size = -0.2
-        self.influence_strength = 0.5
-        self.influence_radius = 1
+        self.step_size = 0.2
+        self.influence_strength = 0
+        self.influence_radius = 100
         self.color = Particle.generate_particle_colors(self.particle_label, 1)[0]  #gets a colorway for this spicific type
         self.shape = Particle.generate_particle_shape(self.particle_label)
 
